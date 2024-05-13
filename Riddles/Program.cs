d8bc8725d53db08b051a91f60afbc87fd6c85e12
fefelovee12 <000940845@mohawkcollege.ca>
2024-05-13 00:31:37 -0400

    ADD VISUAL STUDIO

---
+++ Riddles/Program.cs
@@ -0,0 +1,265 @@
+﻿namespace Riddles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            
+            
+            // INTRO TO "THE RIddLES"
+
+            Console.WriteLine("Hello, Welcome to THE RIddLES!");
+            Console.WriteLine("press Enter To continue");
+            Console.ReadLine();
+
+
+            // These are the of the game RULES
+            Console.WriteLine("Here are the rules!");
+            Console.WriteLine("press enter to continue to each rule ");
+            Console.ReadLine();
+            Console.WriteLine("Read each RIddle");
+            Console.ReadLine();
+            Console.WriteLine("Answer the riddles correctly");
+            Console.ReadLine();
+            Console.WriteLine("See how many you got RIGHT! \nREADY! \nLETS GO!");
+            Console.ReadLine();
+
+            // first riddle
+            Console.WriteLine("THE FIRST RIddLE!");
+            Console.WriteLine("You are in the dark and you only have 3 items. \nA Candle, Matches, and Newspaper.\nwhat are you Lighting first?");
+            
+            string answer = Console.ReadLine();
+
+
+            if (answer == "matches")  
+            { 
+            
+             Console.WriteLine("The answer is correct");
+            
+            }
+            else 
+            { 
+            
+                Console.WriteLine("The answer is incorrect. The correct answer is Matches.");
+
+            }
+                Console.ReadLine();
+
+
+
+            // second riddle
+            Console.WriteLine("THE SECOND RIddLE");
+            Console.WriteLine("It belongs to you, but your friends use it more often. What is it?");
+
+            string answer2 = Console.ReadLine();
+
+
+            if (answer2 == "your name")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is your name.");
+
+            }
+
+                Console.ReadLine();
+
+            
+            // THIRD RIddle
+            Console.WriteLine("THE THIRD RIddLE");
+            Console.WriteLine("What number is odd, until you take away the first letter and it becomes even?");
+           
+            string answer3 = Console.ReadLine();
+
+
+            if (answer3 == "seven")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is seven.");
+
+            }
+                 Console.ReadLine();
+
+
+            // FORTH RIddLE
+            Console.WriteLine("THE FORTH RIddLE");
+            Console.WriteLine("Sean's mother has three childern: Snap, Crackle and ________. Who is the third child?");
+
+            string answer4 = Console.ReadLine();
+
+
+            if (answer4 == "sean")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is sean.");
+
+            }
+            Console.ReadLine();
+
+
+            // FIFTH RIddLE
+            Console.WriteLine("THE FIFTH RIddLE");
+            Console.WriteLine("What has legs, but cant walk?");
+
+            string answer5 = Console.ReadLine();
+
+
+            if (answer5 == "a table")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is a table.");
+
+            }
+            Console.ReadLine();
+
+            // SIXTH RIddLE
+            Console.WriteLine("THE SIXTH RIddLE");
+            Console.WriteLine("what gets wet while drying?");
+
+            string answer6 = Console.ReadLine();
+
+
+            if (answer6 == "a towel")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is a towel.");
+
+            }
+            Console.ReadLine();
+
+            //SEVENTH RIddLE
+            Console.WriteLine("THE SEVENTH RIddLE");
+            Console.WriteLine("What asks no questions but requires many answers?");
+
+            string answer7 = Console.ReadLine();
+
+
+            if (answer7 == "a doorbell ")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is a doorbell.");
+
+            }
+            Console.ReadLine();
+
+            // EIGHTH RIddLE
+            Console.WriteLine("THE EIGHTH RIddLE");
+            Console.WriteLine("What can you put in your pocket that would leave it empty?");
+
+            string answer8 = Console.ReadLine();
+
+
+            if (answer8 == "a hole")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is a hole.");
+
+            }
+            Console.ReadLine();
+
+
+            // NINTH RIddLE
+            Console.WriteLine("THE NINTH RIddLE");
+            Console.WriteLine("What type of car is spelled the same, backwards and forwards?");
+
+            string answer9 = Console.ReadLine();
+
+
+            if (answer9 == "racecar")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is racecar.");
+
+            }
+            Console.ReadLine();
+
+            //TENTH RIddLE
+            Console.WriteLine("THE TENTH RIddLE");
+            Console.WriteLine("If five cats can catch five mice in five minutes, how long will it take a cat to catch one mouse?");
+
+            string answer10 = Console.ReadLine();
+
+
+            if (answer10 == "five minutes")
+            {
+
+                Console.WriteLine("The answer is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The answer is incorrect. The correct answer is five minutes.");
+
+            }
+            Console.ReadLine();
+
+
+
+            // GAME FINISHED
+
+            Console.WriteLine("CONGRATS YOU FINISHED THE RIddLES!");
+
+            
+
+            
+
+
+        }   
+
+
+
+                              
+
+    }
+
+               
+}
